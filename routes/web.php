31f6f8e06fb3c4130e053b5aa28e879729e94038
BXn4 <bencefodor@proton.me>
2025-07-06 22:29:13 +0200

    Added .env.example

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view("welcome");
+});
+
+Route::get("/login", function () {
+    return view("forms/login");
+});
+
+Route::get("/register", function () {
+    return view("forms/register");
 });
